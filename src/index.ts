6128098cb8e0132589e49d0bff89e5f044bf748f
joel-foo <joelfootaimeng@gmail.com>
2022-08-04 13:33:14 +0800

    edited

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
 import routes from './routes/api'
 
 const app = express()
-
-app.use(express.static(__dirname + '/app'))
 
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -17,11 +15,6 @@ app.use(bodyParser.json())
 app.use('/api', routes)
 
 const PORT = process.env.PORT || 3000
-
-//send client page
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(__dirname + '/app/index.html')
-})
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
